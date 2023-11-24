018b22295f9041ef5eb07eab3c66e970265d4757
NarmadhaAllimuthu <narmadhaallimuthu006@gmail.com>
2023-11-24 22:01:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './Product';
+import Cart from './Cart';
+import { UserProvider } from './contex/userContex';
+import Home from './Home';
+import Footer from './Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <UserProvider>
+<NavBar></NavBar>
+
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/product" element={<Product/>}></Route>
+    <Route path="/cart" element={<Cart/>}></Route>
+  </Routes>
+
+    </UserProvider>
+    </BrowserRouter>
   );
 }
 
